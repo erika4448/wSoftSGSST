47587517a1caef18fc51414202e785b375c57e71
PK_er <PK_er@ERIKA-NOTEBOOK>
2016-07-04 19:42:36 -0500

    ajustes proyecto

---
+++ dllSoftSGSST/Sistema/clSisModulo.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Data.Common
+Namespace Sistema
+    Public Class clSisModulo
+        Inherits clBD
+#Region "VARIABLES"
+        Public simoIdModulo As Integer
+        Public simoNombre As String
+        Public simoDescripcion As String
+        Public simoIdEstado As dllSoftSGSST.Sistema.clSisEstado.EnmEstado
+#End Region
+        'FUNCION PARA CARGAR LISTADO DE MODULOS A LOS QUE TIENE ACCESO EL USUARIO
+    End Class
+End Namespace
