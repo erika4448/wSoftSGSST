a8e066f67ba928464ea3450671b97a643dee5f48
PK_er <PK_er@ERIKA-NOTEBOOK>
2016-07-04 19:59:03 -0500

    ajustes

--- dllSoftSGSST/Sistema/clSisModulo.vb
+++ dllSoftSGSST/Sistema/clSisModulo.vb
@@ -9,5 +9,12 @@ Namespace Sistema
         Public simoIdEstado As dllSoftSGSST.Sistema.clSisEstado.EnmEstado
 #End Region
         'FUNCION PARA CARGAR LISTADO DE MODULOS A LOS QUE TIENE ACCESO EL USUARIO
+        Public Function GetTblModuloXIdRelUsuEmp(ByVal parRelIdUsuXEmp As Integer) As Data.DataTable
+            Dim dbCommand As DbCommand = db.GetStoredProcCommand("sis.spSisGetTblModuloXIdRelUsuEmp")
+
+            db.AddInParameter(dbCommand, "parRelIdUsuXEmp", DbType.Int32, parRelIdUsuXEmp)
+
+            Return db.ExecuteDataSet(dbCommand).Tables(0)
+        End Function
     End Class
 End Namespace
---
+++ dllSoftSGSST/Sistema/clSisPagina.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Data.Common
+Namespace Sistema
+    Public Class clSisPagina
+        Inherits clBD
+#Region "VARIABLES"
+        Public sipaIdpagina As Integer
+        Public sipaIdModulo As Integer
+        Public sipaNombre As String
+        Public sipaDescripcion As String
+        Public sipaURLPag As String
+        Public sipaIdEstado As dllSoftSGSST.Sistema.clSisEstado.EnmEstado
+#End Region
+        'FUNCION PARA CARGAR LAS PAGINAS RELACIONADAS AL USUARIO POR MODULO
+        Public Function GetTblInfoPaginaXIdModYRelUsuEmp(ByVal parIdModulo As Integer, ByVal parIdRelUsuXEmp As Integer) As Data.DataTable
+            Dim dbCommand As DbCommand = db.GetStoredProcCommand("sis.spSisGetTblInfoPaginaXIdModYRelUsuEmp")
+
+            db.AddInParameter(dbCommand, "parIdModulo", DbType.Int32, parIdModulo)
+            db.AddInParameter(dbCommand, "parIdRelUsuXEmp", DbType.Int32, parIdRelUsuXEmp)
+
+            Return db.ExecuteDataSet(dbCommand).Tables(0)
+        End Function
+    End Class
+End Namespace
